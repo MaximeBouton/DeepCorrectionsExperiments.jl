551343edbe3cb9ab8538755319f7c9ee08802dfc
MaximeBouton <maxim.bouton@gmail.com>
2019-02-11 18:10:12 -0800

    tweak training script

--- crosswalk_train.jl
+++ crosswalk_train.jl
@@ -62,17 +62,18 @@ rng = MersenneTwister(seed)
 
 const K = 4
 
-pomdp = OCPOMDP(ΔT = 0.5, p_birth = 0.3, max_peds = MAX_PEDS)
+pomdp = OCPOMDP(ΔT = 0.5, p_birth = 0.3, max_peds = MAX_PEDS, γ=0.99, no_ped_prob = 0.3)
 
 env = KMarkovEnvironment(pomdp, k=K)
 
 input_dims = reduce(*, obs_dimensions(env))
-model = Chain(x->flattenbatch(x), Dense(input_dims, 32, relu), Dense(32,32, relu), Dense(32,32, relu), Dense(32,n_actions(env), relu))
+model = Chain(x->flattenbatch(x), Dense(input_dims, 32, relu), Dense(32,32, relu), Dense(32,32, relu), Dense(32,32, relu), Dense(32,n_actions(env), relu))
 dqn_solver = DeepQLearningSolver(qnetwork = model, 
                              learning_rate = 1e-4,
                              max_steps = parsed_args["training_steps"],
-                             eps_end = 0.1,
-                             eps_fraction = 0.5,
+                             eps_end = parsed_args["eps_end"],
+                             eps_fraction = parsed_args["eps_fraction"],
+                             max_episode_length = 100,
                              target_update_freq = 5000,
                              buffer_size = 400_000,
                              train_freq = 4,
@@ -81,8 +82,10 @@ dqn_solver = DeepQLearningSolver(qnetwork = model,
                              prioritized_replay_alpha = 0.7,
                              prioritized_replay_epsilon = 1e-3,
                              eval_freq = 10_000,
+                             save_freq = 10_000,
                              log_freq = 1000,
                              logdir = parsed_args["logdir"],
+                             double_q = true,
                              verbose=true)
 
 if parsed_args["correction"] == nothing 
