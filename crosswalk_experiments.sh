397343bdfb06e5d00804ab3be2f08b6cf1ce902b
MaximeBouton <maxim.bouton@gmail.com>
2019-02-14 14:17:46 -0800

    reorder experiments

--- crosswalk_experiments.sh
+++ crosswalk_experiments.sh
@@ -14,6 +14,17 @@ done
 
 wait
 
+# evaluation
+for expid in 1 2 3 4 5 7 10; do
+    nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid > eval_single$expid.$HOSTNAME.out &
+    echo "nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid > eval_single$expid.$HOSTNAME.out &"
+    nohup julia crosswalk_eval.jl --policy=multi$expid/policy.bson --logdir=multi$expid > eval_multi$expid.$HOSTNAME.out &
+    echo "nohup julia crosswalk_eval.jl --policy=multi$expid/policy.bson --logdir=multi$expid > eval_multi$expid.$HOSTNAME.out &"
+done
+
+echo "Evaluating single policies and muli policies done."
+
+# correction
 for training_budget in 100000 200000 300000 400000 500000 700000 1000000; do
     expid=$((training_budget / 100000))
     nohup julia crosswalk_train.jl --training_steps=$(( training_budget/2)) --eps_fraction=0.1 --correction=single$expid/policy_single.bson --logdir=corr$expid > corr$expid.$HOSTNAME.out &
@@ -22,12 +33,7 @@ done
 
 wait 
 
-# evaluation
 for expid in 1 2 3 4 5 7 10; do
-    nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid > eval_single$expid.$HOSTNAME.out &
-    echo "nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid > eval_single$expid.$HOSTNAME.out &"
-    nohup julia crosswalk_eval.jl --policy=multi$expid/policy.bson --logdir=multi$expid > eval_multi$expid.$HOSTNAME.out &
-    echo "nohup julia crosswalk_eval.jl --policy=multi$expid/policy.bson --logdir=multi$expid > eval_multi$expid.$HOSTNAME.out &"
     nohup julia crosswalk_eval.jl --policy=corr$expid/policy_correction.bson --correction=single$expid/policy_single.bson --logdir=corr$expid > eval_corr$expid.$HOSTNAME.out &
     echo "nohup julia crosswalk_eval.jl --policy=corr$expid/policy_correction.bson --correction=single$expid/policy_single.bson --logdir=corr$expid > eval_corr$expid.$HOSTNAME.out &"
 done
