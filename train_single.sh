4f39ca8264dba4dc454c7a57468a7c5bdfb86d4b
MaximeBouton <maxim.bouton@gmail.com>
2019-02-18 15:51:29 -0800

    change order train single

--- train_single.sh
+++ train_single.sh
@@ -5,7 +5,11 @@ for seed in 1 2 3 4 5; do
         expid=$((training_budget / 100000))
         nohup julia crosswalk_train.jl --training_steps=$(( training_budget/2)) --single --logdir=single$expid-$seed > single$expid.$HOSTNAME.out &
         echo  "nohup julia crosswalk_train.jl --training_steps=$(( training_budget/2)) --single --logdir=single$expid-$seed > single$expid.$HOSTNAME.out &"
+    done
+    wait
+    for expid in 1 2 3 4 5 7 10; do 
         nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid-$seed > eval_single$expid.$HOSTNAME.out &
         echo "nohup julia crosswalk_eval.jl --single --policy=single$expid/policy_single.bson --logdir=single$expid-$seed > eval_single$expid.$HOSTNAME.out &"
     done
+    wait
 done
